936b80051419dceff20a678abaccfa88f5a18832
nevw03 <160777975+nevw03@users.noreply.github.com>
2024-02-21 15:08:27 -0700

    Update cis129_lab02_shortStory.py
    Added story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,7 @@
-print('Hello, World!')
+# Author: Nathaniel Van Woert
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+print("The woman noticed something very wrong one day.")
+print("She looked and noticed her reflection was gone. The room's reflection was backwards.")
+print("Every mirror was the same. The exact same room, reversed reflection. Many under different circumstances.")
+print("That was the last normal day of her life. The day she realized she was a Mirrorshifter.")
